a20fb07e3ae67e1c7d3a58fde8680f8a1fd08772
Max Gonzih <gonzih@gmail.com>
2018-10-23 15:03:45 -0400

    On to something here

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 )
 
@@ -20,19 +21,38 @@ type Pair struct {
 	of2 SearchOffer
 }
 
+func (p Pair) String() string {
+	return fmt.Sprintf("[%.0f %s <- %.0f %s] + [%.0f %s <- %.0f %s] = %f %s",
+		p.of1.SellAmount, p.of1.SellName, p.of1.BuyAmount, p.of1.BuyName,
+		p.of2.SellAmount, p.of2.SellName, p.of2.BuyAmount, p.of2.BuyName,
+		p.of1.Profit(p.of2), p.of1.BuyName,
+	)
+}
+
 func main() {
-	log.Println(currencyNames)
+	// currenciesToScan := []string{"alteration", "fusing", "jeweller's", "chrome", "gcp"}
+	online := true
+	highestProfit := 0.0
+	highestCurrency := ""
 
-	offers1 := searchFor(true, "Delve", "fusing", "chaos")
-	offers2 := searchFor(true, "Delve", "chaos", "fusing")
+	for name := range currencyNames {
+		offers1 := searchFor(online, "Delve", name, "chaos")
+		offers2 := searchFor(online, "Delve", "chaos", name)
 
-	for _, offer1 := range offers1 {
-		for _, offer2 := range offers2 {
-			if offer1.IsProfitable(offer2) {
-				p := Pair{of1: offer1, of2: offer2}
-				log.Println(p)
-				log.Println(p.of1.Profit(p.of2))
+		for _, offer1 := range offers1 {
+			for _, offer2 := range offers2 {
+				if offer1.IsProfitable(offer2) {
+					p := Pair{of1: offer1, of2: offer2}
+					prof := p.of1.Profit(p.of2)
+					if prof > highestProfit {
+						highestProfit = prof
+						highestCurrency = name
+					}
+					log.Println(p.String())
+				}
 			}
 		}
 	}
+
+	log.Printf("Highest profit was with \"%s\" of %f chaos", highestCurrency, highestProfit)
 }
